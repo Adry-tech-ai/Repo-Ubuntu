aab067450d44f58df23d7f740ebdba7a6399eb24
Adry <aaguileram@est.emi.edu.bo>
2025-07-07 00:59:29 -0400

    Se esta agragando la molestia

--- archivo1.py
+++ archivo1.py
@@ -2,3 +2,4 @@ saludo = "Este es el inicio de el uso de GitHub en ubuntu"
 print(saludo)
 mensaje_añadido = "Ademas me apetece enviarte un saludo"
 print(mensaje_añadido)
+print("Me estoy cansando de estos problemas que me da en WSL")
