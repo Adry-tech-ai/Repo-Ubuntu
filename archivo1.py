0b3009c8f6547ae6772b5d41c823982c35efa82f
Adry <aaguileram@est.emi.edu.bo>
2025-07-24 10:01:24 -0400

    Delete archivo1.py

--- archivo1.py
+++
@@ -1,5 +0,0 @@
-saludo = "Este es el inicio de el uso de GitHub en ubuntu"
-print(saludo)
-mensaje_añadido = "Ademas me apetece enviarte un saludo"
-print(mensaje_añadido)
-print("Me estoy cansando de estos problemas que me da en WSL")
